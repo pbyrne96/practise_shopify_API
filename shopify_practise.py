a6bdc7143861eea65add14e316917e7598859804
Philip Byrne <philipbyrne@Philips-MBP.home>
2021-09-15 13:33:09 +0100

    adding changes

--- shopify_practise.py
+++ shopify_practise.py
@@ -8,17 +8,17 @@ FILE_PATH = os.path.join(os.getcwd(),"creditionals.txt")
 
 search_creds = re.compile("=")
 
-
 def santitize_creditonals(file_path) -> Dict[str,str]:
     with open(file_path,"r") as f: creditionals = [i.replace("\n",'') for i in f.readlines()]
-    index_split = lambda chr: search_creds.search(chr).span()[0]
+    index_split = lambda chr: search_creds.search(chr) 
     output={}
     for cred in creditionals:
         indexer = index_split(cred)
-        output.update({cred[:indexer].strip() : cred[indexer+1:].strip()})
+        ins = {cred[:indexer.span()[0]].strip() : cred[indexer.span()[0]+1:].strip()} if indexer else {}
+        output.update(ins)
     yield output        
 
-def access_creds(file_path=FILE_PATH):
+def access_creds(file_path=FILE_PATH) -> Dict[str,str]:
     return next(iter(santitize_creditonals(file_path)))
 
 def check_endpoint(endpoint) -> str:
@@ -30,12 +30,12 @@ def return_endpoint(endpoint) -> Dict[str,str]:
     target_url = creds.get("url")+check_endpoint(endpoint).lower()
     return requests.get(target_url).json()
     
-def update_product_status(endpoint,prod_id,status):
+def update_product_status(endpoint,prod_id,status) -> int:
     creds = access_creds()
-    payload = {"product":{"id": prod_id,"status": status}}
+    payload = {"product":{"status": status}}
     destionation_url = creds.get("url") + check_endpoint(endpoint).lower() + str(prod_id) + ".json"
-    r = requests.put(destionation_url,json=payload)
-    return r.json()
+    return requests.put(destionation_url,json=payload).json()
+
 
 
 if __name__ == "__main__":
