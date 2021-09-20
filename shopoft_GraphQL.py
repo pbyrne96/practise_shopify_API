2ae99201fb13f6e016536cbc318acb0aa9851acb
Philip Byrne <philipbyrne@Philips-MBP.home>
2021-09-20 12:24:50 +0100

    making changes tp graphql shopify

--- shopoft_GraphQL.py
+++ shopoft_GraphQL.py
@@ -1,6 +1,17 @@
 from typing import List,Dict
 from datetime import datetime
+from .shopify_practise import FILE_PATH,change_status
+import re
 
 
 # DOING THE SIMILAT FUNCTIONALITY AS IN shopify practise only with graphQL
 
+class graphQL_interactions(change_status):
+    def __init__(self, file_path: str) -> None:
+        super().__init__(file_path=FILE_PATH)
+    
+    def sanitize_search_term(self,searchTerm:str) ->str:
+        return None
+
+    def fetch_products_by_termß(self,search_term)->str:
+        query = search_term
