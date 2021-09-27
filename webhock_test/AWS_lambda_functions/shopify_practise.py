b08ae180fb2ad0028bcffdb394f84ccfe119db25
Philip Byrne <philipbyrne@philips-mbp.home>
2021-09-27 11:41:07 +0100

    adding as shopify and lambda app

---
+++ webhock_test/AWS_lambda_functions/shopify_practise.py
@@ -0,0 +1,157 @@
+
+import requests 
+import re
+from typing import List,Dict,Callable
+from collections import ChainMap
+import os
+from datetime import datetime
+import math as m
+
+
+FILE_PATH = os.path.join(os.getcwd(),"creditionals.txt")
+
+class change_status:
+    
+    def __init__(self,file_path:str = None) -> None:
+        
+        self.file_path = FILE_PATH if not(file_path) else file_path
+        self.search_creds = re.compile("=")
+        self.search_draft = re.compile("draft")
+        self.search_active = re.compile("active")
+        self.replace_active = "active"
+        self.replace_draft = "draft"
+        self.creds = self.access_creds(self.file_path)
+        self.order = [self.replace_draft,self.replace_active]
+        self.endpoint = '/products/'
+        self.inital_target = "products.json"
+        self.created_at_format = "%Y-%m-%d %H:%M:%S"
+        self.today_diff = lambda strs_date : datetime.strptime(str(datetime.today().strftime(self.created_at_format)),self.created_at_format).day - strs_date.day
+        
+    def santitize_creditonals(self,file_path:str) -> Dict[str,str]:
+        with open(file_path,"r") as f: creditionals = [i.replace("\n",'') for i in f.readlines()]
+        index_split = lambda chr: self.search_creds.search(chr) 
+        for cred in creditionals:
+            indexer = index_split(cred)
+            yield {cred[:indexer.span()[0]].strip() : cred[indexer.span()[0]+1:].strip()} if indexer else {}
+      
+    def access_creds(self,file_path:str) -> Dict[str,str]:
+        dic = dict(ChainMap(*list(self.santitize_creditonals(file_path))))
+        dic["url"] = dic.get("url").format(dic.get("api_key"),dic.get("password"))
+        return dic
+
+    def check_endpoint(self,endpoint) -> str:
+        check_dash = endpoint.find(r"/")
+        return "/" + endpoint + "/" if not(check_dash>=0) else endpoint
+
+    def return_endpoint(self,endpoint:str) -> Dict[str,str]:
+        target_url = self.creds.get("url")+self.check_endpoint(endpoint).lower()
+        return requests.get(target_url).json()
+
+    def check_low_inventory_status(self,lim:int=10):
+        products_data = self.return_endpoint(self.inital_target)
+        l = products_data[self.inital_target.split(".")[0]]
+        output = {}
+        [output.update(l[idx]) for idx in range(len(l)) if (l[idx].get("variants")[0].get("inventory_quantity")) < lim]
+        return self.change_status(self.get_ids_by_status({"products":[output]},self.search_active)) if output else output
+
+    def caclulate_ROS(self,units_sold , oriingal_amount) -> float:
+        try:
+            return (units_sold/oriingal_amount)  * 100
+        except ZeroDivisionError:
+            return 0.0
+
+    def apply_lower_price(self,price:float,discount:float=.10) -> float:
+        return float(price) - float(price) * discount
+
+    def get_low_rate_of_sale(self,
+                            lim_days=1,
+                            lim_ros=80.0) -> List[Dict[str,str]]:
+                            
+        products_data = self.return_endpoint(self.inital_target)
+        for_discount=[]
+        for idx in range(len(products_data.get("products"))):
+            
+            curr = products_data.get("products")[idx].get("variants")[0]
+            strs = curr.get("created_at")
+            period_selling = self.today_diff(datetime.strptime(strs.replace(strs[strs.find(\
+                "".join(i for i in strs if i.isalpha()))]," ").split("+")[0] , self.created_at_format))
+            ros = self.caclulate_ROS(curr.get("inventory_quantity"),curr.get("old_inventory_quantity"))
+
+            if (period_selling >= lim_days ) and (ros >= lim_ros):
+                for_discount.append(products_data.get("products")[idx])
+
+        return for_discount
+    
+    def insert_price_change(self)->None:
+        for_discount = self.get_low_rate_of_sale()
+        for d in for_discount:
+            update = d.get("variants")[0]
+            update["price"] = self.apply_lower_price(update.get("price"))
+            yield update
+        
+    def access_price_change_staging(self):
+        return list(self.insert_price_change())
+    
+    def apply_price_changes(self):
+        return [self.update_prices(self.endpoint,str(d.get("product_id")),{"product":{"variant":d }})\
+            for d in self.access_price_change_staging() ]
+
+    def get_ids_by_status(self,
+                          ids_list:List[Dict[str,str]],
+                          search_pattern: re.Pattern = None)->List[str]:
+
+        search_pattern = self.search_draft if not(search_pattern) else search_pattern
+        target_keys=['id','status']
+        return [i for i in  (", ".join(str(d.get(i)) for i in target_keys) for d in ids_list["products"]) if search_pattern.search(i)]
+
+    def format_ids_for_staging(self,
+                              ids_not_active:List[str],
+                              draft_replace:bool=True)-> List[str]:
+        if not(draft_replace): self.order=self.order[::-1]
+        return [i.replace(self.order[0],self.order[-1]).split(",") for i in ids_not_active]
+        
+    def update_product_status(self,
+                             endpoint:str,
+                             prod_id:str,
+                             status:str)\
+                             -> Dict[str,str]:
+        
+        payload = {"product":{"status": status}} 
+        destionation_url = self.creds.get("url") + self.check_endpoint(endpoint).lower() + str(prod_id) + ".json"
+        return self.submit_put_request(destionation_url,payload)
+
+    def update_prices(self,
+                    endpoint:str,
+                    prod_id:str,
+                    payload:Dict[str,str]) -> Dict[str,str]:
+
+        destination_url = self.creds.get("url") + endpoint.lower() + str(prod_id) + ".json"
+        return self.submit_put_request(destination_url , payload)
+
+    def submit_put_request(self,destionation_url:str,payload:Dict[str,str]):
+        r = requests.put(destionation_url,json=payload)
+        try:
+            return r.json()
+        except Exception as e:
+            return r
+
+    def change_status(self,staging_list:List[str]) ->List[Dict[str,str]]:
+        format_for_staging = self.format_ids_for_staging(staging_list,False)
+        return [access.update_product_status(self.endpoint,*list(map(str.strip,params))) for params in format_for_staging]
+    
+    def define_replen_rules(self,n:int,limit:int)->int:
+        import numpy as np
+        find_nth_lim = n ** (np.log(n) // 2) 
+        find_sin = m.sin(m.sqrt(find_nth_lim)) ** .5
+        return round(limit ** (find_nth_lim//find_sin))
+
+    def __hash__(self,__int: int) -> int:
+        return  hash(str((__int**.5)%97*100))
+
+if __name__ == "__main__":
+
+    access = change_status()
+    print(access.apply_price_changes())
+
+
+ 
---
+++ webhock_test/AWS_lambda_functions/web_hooks.py
@@ -0,0 +1,145 @@
+from flask import   Flask,request, abort
+# from flask_lambda import FlaskLambda
+import hmac
+import hashlib
+import base64
+from shopify_practise import change_status
+import os
+import json
+import re
+import os
+from datetime import datetime
+
+# import shopify
+
+app = Flask(__name__)
+
+access = change_status().creds
+SECRET = bytes(access.get("SECRET"),'utf-8')
+HMAC_STR = 'X-Shopify-Hmac-SHA256'
+
+shop_url = access.get("url")
+product_update_file = os.getcwd()+"/staging.json"
+order_update_file = os.getcwd()+"/order_update.json"
+customer_records_file = os.getcwd() + "/customer_records.json"
+
+cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+verify_token = lambda data, r: verify_webhook(data,r.headers.get(HMAC_STR))
+clean_data = lambda data : json.loads(data.decode('utf-8'))
+target = 'sku'
+grams_to_kilos = 0.001
+datetime_format = "%Y-%m-%d %H:%M:%S"
+msg = "Webhook received.",200
+
+def submit_file(file_to_submit,data):
+    with open(file_to_submit,"w") as f: json.dump(data,f,indent=1)
+
+def verify_webhook(data, hmac_header ):
+    digest = hmac.new(SECRET, data , hashlib.sha256).digest()
+    computed_hmac = base64.b64encode(digest)
+    return hmac.compare_digest(computed_hmac, hmac_header.encode('utf-8'))
+
+def activate_response(request):
+    data = request.get_data()
+    verified = verify_token(data,request)
+    if not verified:
+        abort(401)
+
+    return clean_data(data)
+
+@app.route('/APP/eight/webhook/product_update', methods=['POST'])
+def handle_webhook(file_to_submit=product_update_file):
+    data = activate_response(request)
+    data['id'] = int(data.get('id'))
+    data['body_html'] = "".join(i if ord(i) < 128 else '' for i in re.sub(cleanr,'',data.get('body_html')))\
+                                                                if data['body_html'] else data['body_html']
+    submit_file(file_to_submit,data)
+    return msg
+
+@app.route('/APP/eight/webhook/order_update',methods=['POST'])
+def order_update(file_to_submit=order_update_file):
+    data = activate_response(request)
+    submit_file(file_to_submit,data)
+    return (200,data)
+    
+def get_data_for_profitability_analysis(file_name=order_update_file):
+    if file_name not in os.listdir(): assert ("file not in directory")
+    
+    with open(file_name,"r") as f: data = json.load(f)
+
+    items_updated = iter(data.get("line_items"))
+    is_discount,order_id,time_stamp = data.get("discount_codes"),data.get("id"),data.get("created_at")
+    targets= ("sku","price","grams","total_discount","quantity")
+    this_order_meta={}
+
+    while True:
+        try:
+            curr = next(items_updated)
+            t = {curr.get(targets[0]):{k:curr.get(k) for k in targets[1:]}}
+            this_order_meta.update(t)
+        except StopIteration:
+            break
+    
+    this_order_meta["created_at"] = time_stamp
+    t = this_order_meta["discount_codes"] = is_discount if is_discount else None
+
+    return {order_id:this_order_meta}
+
+def strip_to_datetime(dateString):
+    return datetime.strptime(" ".join(re.split("[T+]",dateString)[:-1]),datetime_format).strftime(datetime_format)
+
+def calc_net_prof_weight(dic):
+
+    net_profit,weights = 0,0
+
+    for k in list(dic.keys()):
+        if target in k:
+            params = dic.get(k)
+            net_profit += params[0]
+            weights += params[-1]
+            dic.pop(k)
+    dic["prof_weight"] = (net_profit,weights)
+    return dic
+
+def insert_bundle_data_for_insertion():
+
+    most_recent_addition = get_data_for_profitability_analysis()
+    order_profitability = {}
+    for key,value in most_recent_addition.get(*list(most_recent_addition.keys())).items():
+        if key.isdigit():
+            weight = value.get("grams") * grams_to_kilos
+            value.pop('grams')
+            net_profit = eval(f"""{value.get("price")} - {value.get("total_discount")} * {value.get("quantity")}""")
+            order_profitability[target+"_"+key] = [net_profit,weight]
+        else:
+            order_profitability[key] = value
+
+    order_profitability["created_at"] = strip_to_datetime(order_profitability.get('created_at'))
+
+    return {list(most_recent_addition.keys())[0] : calc_net_prof_weight(order_profitability)}
+
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+
+    request_type = event.get('httpMethod')
+    
+    if request_type in ['GET']:
+        payload =  {
+            'statusCode': 200,
+            'body': json.dumps('Hello from Lambda!')
+        }
+    elif request_type in ["POST"]:
+        payload = {
+            'statusCode': 500,
+            'body': json.dumps("feck off")
+        }
+    return payload
+
+if __name__ == "__main__":
+    #app.run(debug=True)
+    print("*"*20)
+    print(insert_bundle_data_for_insertion())
+
+      
